da9b1141a9b73418ce512319f55a845eff7787a5
Josh Fisher <jfisherd@gmail.com>
2026-01-02 12:30:14 -0500

    change appHeader component to PascalCase

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './styles.css'
+import { AppHeader } from './components/appHeader'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <AppHeader />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- src/components/appHeader.tsx
+++ src/components/appHeader.tsx
@@ -1,5 +1,4 @@
-
-export function appHeader() {
+export function AppHeader() {
 
     return (
         <>
